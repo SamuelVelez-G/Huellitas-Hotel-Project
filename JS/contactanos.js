27ece40ccc6dc55bdb5f200a8b601dbe63961b9e
Mjdiazri <citruss2012@hotmail.es>
2026-08-03 20:10:18 -0500

    Se ajusta textarea y se inicia codigo de validacion

--- JS/contactanos.js
+++ JS/contactanos.js
@@ -24,4 +24,42 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 //Funcion validar datos formulario
-document.getElementById('')
+//Input
+const formulario = document.querySelector('#formularioContactanos');
+const nombreInput = document.querySelector('#inputNombreForm');
+const apellidoInput = document.querySelector('#inputApellidoForm');
+const correo = document.querySelector('#inputEmail');
+const telwps = (document.querySelector('#inputTelefonoWSForm'));
+const tel = (document.querySelector('#inputTelefono'));
+const categoria = document.querySelector('#inputCategoria');
+//Contenedores
+const divNombreInput = document.querySelector('.divNombreForm');
+
+
+const expresionesRegex = {
+    nombreApellidos : /^[a-zñáéíóú ]+$/i,
+    correoE : /^[a-z0-9_.+-]+@[a-z0-9-]+\.[a-z0-9-.]+$/i,
+    telefonos : /^\d{10}$/ 
+}
+
+function validarCampos(input){
+   
+    switch (input){
+        case input:
+            if(expresionesRegex.nombreApellidos.test(input.value)){
+                input.classList.remove('inputIncorrecto') 
+                input.classList.add('inputCorrecto') 
+                                  
+            } else {
+                input.classList.remove('inputCorrecto') 
+                input.classList.add('inputIncorrecto');                
+                let mensaje = document.createElement('p');
+                mensaje.textContent= '❌Por favor ingresa solo letras'
+                divNombreInput.appendChild(mensaje)
+            }              
+    }
+}
+
+
+
+
