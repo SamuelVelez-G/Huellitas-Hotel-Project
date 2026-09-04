4e9f86b0f0d669a573e49c717e29c056f62f9015
SamuelVelez-G <samu2005vel@gmail.com>
2026-09-04 13:02:43 -0500

    Correcion

--- JS/login.js
+++ JS/login.js
@@ -76,7 +76,7 @@ function cambiarModo(registro) {
 
 function destinoDespuesDeIniciar() {
     const destino = new URLSearchParams(window.location.search).get('redirect');
-    return destino || './index.html';
+    return destino || '/index.html';
 }
 
 loginTab.addEventListener('click', () => cambiarModo(false));
